2b9150ca51839c57a0dc80a92b0b8a6f5d110eca
meiheW <1925257530@qq.com>
2020-08-16 22:48:42 +0800

    Semaphore

---
+++ sync/src/main/java/com/example/demo/lock/SemaphoreTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.lock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author meihewang
+ * @date 2020/08/16  19:52
+ */
+public class SemaphoreTest {
+
+    private static final int THREAD_COUNT = 30;
+
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
+
+    private static Semaphore s = new Semaphore(10);
+
+    public static void main(String[] args) {
+        for(int i=0; i<THREAD_COUNT; i++){
+            threadPool.execute(new Runnable() {
+                                   @Override
+                                   public void run() {
+                                       try{
+                                           s.acquire();
+                                           System.out.println("save data");
+                                           //s.release();
+                                       }catch (Exception e){
+
+                                       }
+                                   }
+                               }
+
+            );
+        }
+        threadPool.shutdown();
+    }
+
+}
