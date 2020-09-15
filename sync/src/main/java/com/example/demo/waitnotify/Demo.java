1ab12bfd555804137a6178f6db2504ddeec66102
meiheW <1925257530@qq.com>
2020-09-15 22:44:58 +0800

    schronized()

---
+++ sync/src/main/java/com/example/demo/waitnotify/Demo.java
@@ -0,0 +1,18 @@
+package com.example.demo.waitnotify;
+
+/**
+ * @author meihewang
+ * @date 2020/09/15  22:03
+ */
+public class Demo {
+
+    public static void main(String[] args) throws InterruptedException {
+        String s = "hello";
+        
+        //会报异常，必须和synchronized共用
+        s.wait();
+    }
+
+
+
+}
